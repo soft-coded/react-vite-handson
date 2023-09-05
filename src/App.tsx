f84222ca51ce71658f8b8df237750b0fd615e685
shrutanten <shrutanten.work@gmail.com>
2023-09-05 12:06:35 +0530

    Make wrapper flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div>
+		<div className="wrapper">
 			<button onClick={() => setCount(count + 1)}>INC</button>
 			<h1>{count}</h1>
 			<button onClick={() => setCount(count - 1)}>DEC</button>
